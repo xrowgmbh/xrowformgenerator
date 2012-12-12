e11105ede9880140380762542c9894369f7d1de9
XROW System Account <xrow@test01.hande.c.de.siteprotect.eu>
2012-12-12 12:00:28 +0100

    transaltion added

---
+++ translations/pol-PL/translation.ts
@@ -0,0 +1,448 @@
+<!DOCTYPE TS>
+<TS><context>
+    <name>design/base</name>
+    <message>
+      <source>Please insert the signs of the image below</source>
+      <translation>Wpisz poniżej znaki widoczne na obrazie</translation>
+      
+    </message>
+    <message>
+      <source>Send form</source>
+      <translation>Wyślij formularz</translation>
+      
+    </message>
+    <message>
+      <source>Javascript must be enabled to submit this form.</source>
+      <translation>Aby przesłać formularz, należy włączyć Javascript.</translation>
+      
+    </message>
+  </context><context>
+    <name>design/ezwebin/collectedinfo/form</name>
+    <message>
+      <source>Form %formname</source>
+      <translation>Formularz %formname</translation>
+      
+    </message>
+    <message>
+      <source>You have already submitted this form. The data you entered was:</source>
+      <translation>Ten formularz został już przez Ciebie przesłany. Wpisana data to:</translation>
+      
+    </message>
+    <message>
+      <source>Return to site</source>
+      <translation>Wróć na stronę</translation>
+      
+    </message>
+  </context><context>
+    <name>kernel/classes/datatypes</name>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="18"/>
+      <source>Form generator</source>
+      <comment>Datatype name</comment>
+      <translation>Generator formularzy</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="462"/> <location filename="xrowformgeneratortype.php" line="481"/> <location filename="xrowformgeneratortype.php" line="529"/>
+      <source>Input required.</source>
+      <translation>Należy wprowadzić dane.</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="490"/>
+      <source>E-mail address is not valid.</source>
+      <translation>Niepoprawny adres e-mail.</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="509"/>
+      <source>You need to select this checkbox.</source>
+      <translation>Należy zaznaczyć to pole wyboru.</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="541"/>
+      <source>Please enter a valid number.</source>
+      <translation>Podaj poprawny numer.</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="583"/> <location filename="xrowformgeneratortype.php" line="593"/>
+      <source>Please select at least one option.</source>
+      <translation>Wybierz co najmniej jedną opcję.</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="622"/>
+      <source>Please add a valid file for upload.</source>
+      <translation>Dodaj poprawny plik do przesłania.</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="629"/>
+      <source>The uploaded file is too big. Please select a smaller one.</source>
+      <translation>Plik do przesłania jest zbyt duży. Wybierz mniejszy plik.</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="676"/>
+      <source>Spam protection. The signs of the captcha code didn't match. Please enter the correct code of the image at the bottom.</source>
+      <translation>Ochrona przed spamem. Znaki kodu captcha są inne niż znaki na obrazku. Wpisz poprawny kod widoczny na obrazku poniżej.</translation>
+      
+    </message>
+  </context><context>
+    <name>xrowformgenerator/edit</name>
+    <message>
+      <source>No form fields defined.</source>
+      <translation>Brak zdefiniowanych pól formularza.</translation>
+      
+    </message>
+    <message>
+      <source>One ore more errors at the input occured. Please correct the input of the fields which are marked red.</source>
+      <translation>Podczas wprowadzania wystąpił co najmniej jeden błąd. Popraw dane wprowadzone w polach zaznaczonych na czerwono.</translation>
+      
+    </message>
+    <message>
+      <source>Form is using a captcha</source>
+      <translation>Formularz używa kodu captcha</translation>
+      
+    </message>
+    <message>
+      <source>Show the amount of forms in the mail</source>
+      <translation>Pokaż liczbę formularzy w poczcie</translation>
+      
+    </message>
+    <message>
+      <source>Display Data After Submit</source>
+      <translation>Wyświetl dane po ich wprowadzeniu</translation>
+      
+    </message>
+    <message>
+      <source>Receiver(separate multiple Emails with a semicolon)</source>
+      <translation>Odbiorca (oddziel adresy e-mail średnikami)</translation>
+      
+    </message>
+    <message>
+      <source>Subject</source>
+      <translation>Temat</translation>
+      
+    </message>
+    <message>
+      <source>Sender</source>
+      <translation>Nadawca</translation>
+      
+    </message>
+    <message>
+      <source>Delete option</source>
+      <translation>Usuń opcję</translation>
+      
+    </message>
+    <message>
+      <source>Option</source>
+      <translation>Opcja</translation>
+      
+    </message>
+    <message>
+      <source>Click here to select this value by default</source>
+      <translation>Kliknij tu, aby wybierać tę wartość domyślnie</translation>
+      
+    </message>
+    <message>
+      <source>Default value</source>
+      <translation>Wartość domyślna</translation>
+      
+    </message>
+    <message>
+      <source>Move up</source>
+      <translation>Przesuń do góry</translation>
+      
+    </message>
+    <message>
+      <source>Move down</source>
+      <translation>Przesuń w dół</translation>
+      
+    </message>
+    <message>
+      <source>Select image</source>
+      <translation>Wybierz obraz</translation>
+      
+    </message>
+    <message>
+      <source>String input field</source>
+      <translation>Pole do wprowadzenia ciągu znaków</translation>
+      
+    </message>
+    <message>
+      <source>Delete form element.</source>
+      <translation>Usuń element formularza.</translation>
+      
+    </message>
+    <message>
+      <source>Name</source>
+      <translation>Imię</translation>
+      
+    </message>
+    <message>
+      <source>Description</source>
+      <translation>Opis</translation>
+      
+    </message>
+    <message>
+      <source>Use this checkbox if the input of this form field is required.</source>
+      <translation>Użyj tego pola wyboru, jeśli wymagane jest wprowadzenie danych tego pola formularza.</translation>
+      
+    </message>
+    <message>
+      <source>Required</source>
+      <translation>Wymagane</translation>
+      
+    </message>
+    <message>
+      <source>Description input field</source>
+      <translation>Pole do wprowadzenia opisu</translation>
+      
+    </message>
+    <message>
+      <source>Hidden input field</source>
+      <translation>Pole do wprowadzenia ukrytych danych</translation>
+      
+    </message>
+    <message>
+      <source>Value</source>
+      <translation>Wartość</translation>
+      
+    </message>
+    <message>
+      <source>Spacer</source>
+      <translation>Spacja</translation>
+      
+    </message>
+    <message>
+      <source>Text input field</source>
+      <translation>Pole do wprowadzania tekstu</translation>
+      
+    </message>
+    <message>
+      <source>Number input field</source>
+      <translation>Pole do wprowadzania cyfr</translation>
+      
+    </message>
+    <message>
+      <source>Use this checkbox if the input of this form field should be validated.</source>
+      <translation>Użyj tego pola wyboru, jeśli dane wprowadzone w tym polu formularza powinny zostać potwierdzone.</translation>
+      
+    </message>
+    <message>
+      <source>Input requires validation</source>
+      <translation>Wprowadzane dane wymagają potwierdzenia</translation>
+      
+    </message>
+    <message>
+      <source>Checkbox input field</source>
+      <translation>Pole do wprowadzania pola wyboru</translation>
+      
+    </message>
+    <message>
+      <source>Use this checkbox if the checkbox should be selected by default.</source>
+      <translation>Użyj tego pola wyboru, jeśli pole wyboru ma być zaznaczane domyślnie.</translation>
+      
+    </message>
+    <message>
+      <source>Email input field</source>
+      <translation>Pole do wprowadzanie adresu e-mail</translation>
+      
+    </message>
+    <message>
+      <source>Options input field</source>
+      <translation>Pole do wprowadzania opcji</translation>
+      
+    </message>
+    <message>
+      <source>Choose option type</source>
+      <translation>Wybierz rodzaj opcji</translation>
+      
+    </message>
+    <message>
+      <source>Choose how to display your option list in the form.</source>
+      <translation>Wybierz sposób wyświetlania listy opcji w formularzu.</translation>
+      
+    </message>
+    <message>
+      <source>Radio button list - 1 option is choosable</source>
+      <translation>Lista przycisków radiowych — można zaznaczyć 1 opcję</translation>
+      
+    </message>
+    <message>
+      <source>Checkbox list - all options are choosable</source>
+      <translation>Lista pól wyboru — można zaznaczyć wszystkie opcje</translation>
+      
+    </message>
+    <message>
+      <source>Select box - 1 option is choosable</source>
+      <translation>Pole wyboru — można zaznaczyć 1 opcję</translation>
+      
+    </message>
+    <message>
+      <source>Select box - all options are choosable</source>
+      <translation>Pole wyboru — można zaznaczyć wszystkie opcje</translation>
+      
+    </message>
+    <message>
+      <source>Add option</source>
+      <translation>Dodaj opcję</translation>
+      
+    </message>
+    <message>
+      <source>A click on this button adds one option.</source>
+      <translation>Kliknięcie przycisku powoduje dodanie jednej opcji.</translation>
+      
+    </message>
+    <message>
+      <source>Upload input field</source>
+      <translation>Pole przesyłania danych</translation>
+      
+    </message>
+    <message>
+      <source>Options with image input field</source>
+      <translation>Opcje pola przesyłania danych</translation>
+      
+    </message>
+    <message>
+      <source>Add a form field</source>
+      <translation>Dodaj pole formularza</translation>
+      
+    </message>
+    <message>
+      <source>string</source>
+      <translation>ciąg</translation>
+      
+    </message>
+    <message>
+      <source>text</source>
+      <translation>tekst</translation>
+      
+    </message>
+    <message>
+      <source>number</source>
+      <translation>liczba</translation>
+      
+    </message>
+    <message>
+      <source>checkbox</source>
+      <translation>pole wyboru</translation>
+      
+    </message>
+    <message>
+      <source>options</source>
+      <translation>opcje</translation>
+      
+    </message>
+    <message>
+      <source>options with image</source>
+      <translation>opcje z obrazem</translation>
+      
+    </message>
+    <message>
+      <source>email</source>
+      <translation>e-mail</translation>
+      
+    </message>
+    <message>
+      <source>upload</source>
+      <translation>przesyłanie</translation>
+      
+    </message>
+    <message>
+      <source>hidden</source>
+      <translation>ukryte</translation>
+      
+    </message>
+    <message>
+      <source>spacer</source>
+      <translation>spacja</translation>
+      
+    </message>
+    <message>
+      <source>description</source>
+      <translation>opis</translation>
+      
+    </message>
+    <message>
+      <source>Add element</source>
+      <translation>Dodaj element</translation>
+      
+    </message>
+    <message>
+      <source>Add a new form element</source>
+      <translation>Dodaj nowy element formularza</translation>
+      
+    </message>
+    <message>
+      <source>Using captcha for input.</source>
+      <translation>Kod captcha wymagany przed wprowadzeniem danych.</translation>
+      
+    </message>
+    <message>
+      <source>No captcha for input.</source>
+      <translation>Kod captcha niewymagany przed wprowadzeniem danych.</translation>
+      
+    </message>
+    <message>
+      <source>Please fill out the form. The input of fields which are marked with a asterisk (*) is required.</source>
+      <translation>Wypełnij formularz. Wymagane jest wypełnienie pól oznaczonych gwiazdką (*).</translation>
+      
+    </message>
+  </context><context>
+    <name>xrowformgenerator/kernel/classes/datatypes</name>
+    <message>
+      <source>Input required</source>
+      <translation>Należy wprowadzić dane</translation>
+      
+    </message>
+  </context><context>
+    <name>xrowformgenerator/mail</name>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="850"/> <location line="0"/>
+      <source>No.:</source>
+      <translation>Nr</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="851"/>
+      <source>Date</source>
+      <translation>Data</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="910"/> <location line="0"/>
+      <source>Yes</source>
+      <translation>Tak</translation>
+      
+    </message>
+    <message>
+      <location filename="xrowformgeneratortype.php" line="914"/> <location line="0"/>
+      <source>No</source>
+      <translation>Nie</translation>
+      
+    </message>
+    <message>
+      <source>Form sent successfully</source>
+      <translation>Formularz został wysłany</translation>
+      
+    </message>
+    <message>
+      <source>Thank You! You have successfully submitted the form.</source>
+      <translation>Dziękujemy! Udało się wysłać formularz.</translation>
+      
+    </message>
+    <message>
+      <source>Your data</source>
+      <translation>Twoje dane</translation>
+      
+    </message>
+    <message>
+      <source>required</source>
+      <translation>wymagane</translation>
+      
+    </message>
+  </context></TS>
