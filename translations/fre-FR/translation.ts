11b9609b478c87a24f6d63b486a57bfc02a8ef8c
StephanBogansky <stephan@xrow.de>
2012-12-31 09:17:46 +0100

    translation correction fre added

--- translations/fre-FR/translation.ts
+++ translations/fre-FR/translation.ts
@@ -2,34 +2,34 @@
 <TS><context>
     <name>design/base</name>
     <message>
-      <source>Veuillez insérer les signes de l’image ci-dessous</source>
+      <source>Please insert the signs of the image below</source>
       <translation>Veuillez insérer les signes de l’image ci-dessous</translation>
       
     </message>
     <message>
-      <source>Envoyer le formulaire</source>
+      <source>Send form</source>
       <translation>Envoyer le formulaire</translation>
       
     </message>
     <message>
-      <source>Javascript doit être activé pour soumettre ce formulaire.</source>
+      <source>Javascript must be enabled to submit this form.</source>
       <translation>Javascript doit être activé pour soumettre ce formulaire.</translation>
       
     </message>
   </context><context>
     <name>design/ezwebin/collectedinfo/form</name>
     <message>
-      <source>Formulaire <noxl start="%" name="formname" end="">nom du formulaire</noxl></source>
-      <translation>Formulaire </translation>
+      <source>Form %formname</source>
+      <translation>Formulaire %formname</translation>
       
     </message>
     <message>
-      <source>Vous avez déjà soumis ce formulaire. Les données que vous avez saisies étaient :</source>
+      <source>You have already submitted this form. The data you entered was:</source>
       <translation>Vous avez déjà soumis ce formulaire. Les données que vous avez saisies étaient :</translation>
       
     </message>
     <message>
-      <source>Retour au site</source>
+      <source>Return to site</source>
       <translation>Retour au site</translation>
       
     </message>
@@ -37,106 +37,103 @@
     <name>kernel/classes/datatypes</name>
     <message>
       <location filename="xrowformgeneratortype.php" line="18"/>
-      <source>Générateur de formulaire</source>
-      <comment>Nom du type de données</comment>
+      <source>Form generator</source>
+      <comment>Datatype name</comment>
       <translation>Générateur de formulaire</translation>
       
     </message>
     <message>
-      <location filename="xrowformgeneratortype.php" line="462"/>
-      <location filename="xrowformgeneratortype.php" line="481"/>
-      <location filename="xrowformgeneratortype.php" line="529"/>
-      <source>Entrée requise.</source>
+      <location filename="xrowformgeneratortype.php" line="462"/> <location filename="xrowformgeneratortype.php" line="481"/> <location filename="xrowformgeneratortype.php" line="529"/>
+      <source>Input required.</source>
       <translation>Entrée requise.</translation>
       
     </message>
     <message>
       <location filename="xrowformgeneratortype.php" line="490"/>
-      <source>L’adresse e-mail n’est pas valide.</source>
+      <source>E-mail address is not valid.</source>
       <translation>L’adresse e-mail n’est pas valide.</translation>
       
     </message>
     <message>
       <location filename="xrowformgeneratortype.php" line="509"/>
-      <source>Vous devez cocher cette case.</source>
+      <source>You need to select this checkbox.</source>
       <translation>Vous devez cocher cette case.</translation>
       
     </message>
     <message>
       <location filename="xrowformgeneratortype.php" line="541"/>
-      <source>Veuillez entrer un chiffre valide.</source>
+      <source>Please enter a valid number.</source>
       <translation>Veuillez entrer un chiffre valide.</translation>
       
     </message>
     <message>
-      <location filename="xrowformgeneratortype.php" line="583"/>
-      <location filename="xrowformgeneratortype.php" line="593"/>
-      <source>Veuillez sélectionner au moins une option.</source>
+      <location filename="xrowformgeneratortype.php" line="583"/> <location filename="xrowformgeneratortype.php" line="593"/>
+      <source>Please select at least one option.</source>
       <translation>Veuillez sélectionner au moins une option.</translation>
       
     </message>
     <message>
       <location filename="xrowformgeneratortype.php" line="622"/>
-      <source>Veuillez ajouter un fichier valide pour le téléchargement.</source>
+      <source>Please add a valid file for upload.</source>
       <translation>Veuillez ajouter un fichier valide pour le téléchargement.</translation>
       
     </message>
     <message>
       <location filename="xrowformgeneratortype.php" line="629"/>
-      <source>Le fichier téléchargé est trop volumineux. Veuillez en sélectionner un plus petit.</source>
+      <source>The uploaded file is too big. Please select a smaller one.</source>
       <translation>Le fichier téléchargé est trop volumineux. Veuillez en sélectionner un plus petit.</translation>
       
     </message>
     <message>
       <location filename="xrowformgeneratortype.php" line="676"/>
-      <source>Protection contre les spams. Les signes du code captcha ne correspondaient pas. Veuillez saisir le code correct de l’image en bas.</source>
+      <source>Spam protection. The signs of the captcha code didn't match. Please enter the correct code of the image at the bottom.</source>
       <translation>Protection contre les spams. Les signes du code captcha ne correspondaient pas. Veuillez saisir le code correct de l’image en bas.</translation>
       
     </message>
   </context><context>
     <name>xrowformgenerator/edit</name>
     <message>
-      <source>Aucun champ de formulaire défini.</source>
+      <source>No form fields defined.</source>
       <translation>Aucun champ de formulaire défini.</translation>
       
     </message>
     <message>
-      <source>Une ou plusieurs erreurs se sont produites lors de l’entrée. Veuillez corriger l’entrée des champs marqués en rouge.</source>
+      <source>One ore more errors at the input occured. Please correct the input of the fields which are marked red.</source>
       <translation>Une ou plusieurs erreurs se sont produites lors de l’entrée. Veuillez corriger l’entrée des champs marqués en rouge.</translation>
       
     </message>
     <message>
-      <source>Le formulaire utilise un captcha</source>
+      <source>Form is using a captcha</source>
       <translation>Le formulaire utilise un captcha</translation>
       
     </message>
     <message>
-      <source>Montrer la quantité de formulaires dans le mail</source>
+      <source>Show the amount of forms in the mail</source>
       <translation>Montrer la quantité de formulaires dans le mail</translation>
       
     </message>
     <message>
-      <source>Afficher les données après soumission</source>
+      <source>Display Data After Submit</source>
       <translation>Afficher les données après soumission</translation>
       
     </message>
     <message>
-      <source>Récepteur (séparer adresses multiples par un point-virgule)</source>
+      <source>Receiver(separate multiple Emails with a semicolon)</source>
       <translation>Récepteur (séparer adresses multiples par un point-virgule)</translation>
       
     </message>
     <message>
-      <source>Objet</source>
+      <source>Subject</source>
       <translation>Objet</translation>
       
     </message>
     <message>
-      <source>Expéditeur</source>
+      <source>Sender</source>
       <translation>Expéditeur</translation>
       
     </message>
     <message>
-      <source>Option de suppression</source>
+      <source>Delete option</source>
       <translation>Option de suppression</translation>
       
     </message>
@@ -146,42 +143,42 @@
       
     </message>
     <message>
-      <source>Cliquez ici pour sélectionner cette valeur par défaut</source>
+      <source>Click here to select this value by default</source>
       <translation>Cliquez ici pour sélectionner cette valeur par défaut</translation>
       
     </message>
     <message>
-      <source>Valeur par défaut</source>
+      <source>Default value</source>
       <translation>Valeur par défaut</translation>
       
     </message>
     <message>
-      <source>Monter</source>
+      <source>Move up</source>
       <translation>Monter</translation>
       
     </message>
     <message>
-      <source>Descendre</source>
+      <source>Move down</source>
       <translation>Descendre</translation>
       
     </message>
     <message>
-      <source>Sélectionner l’image</source>
+      <source>Select image</source>
       <translation>Sélectionner l’image</translation>
       
     </message>
     <message>
-      <source>Champ de saisie de chaîne</source>
+      <source>String input field</source>
       <translation>Champ de saisie de chaîne</translation>
       
     </message>
     <message>
-      <source>Supprimer élément de formulaire.</source>
+      <source>Delete form element.</source>
       <translation>Supprimer élément de formulaire.</translation>
       
     </message>
     <message>
-      <source>Nom</source>
+      <source>Name</source>
       <translation>Nom</translation>
       
     </message>
@@ -191,147 +188,147 @@
       
     </message>
     <message>
-      <source>Cochez cette case si l’entrée de ce champ de formulaire est requise.</source>
+      <source>Use this checkbox if the input of this form field is required.</source>
       <translation>Cochez cette case si l’entrée de ce champ de formulaire est requise.</translation>
       
     </message>
     <message>
-      <source>Requis</source>
+      <source>Required</source>
       <translation>Requis</translation>
       
     </message>
     <message>
-      <source>Champ de saisie de description</source>
+      <source>Description input field</source>
       <translation>Champ de saisie de description</translation>
       
     </message>
     <message>
-      <source>Champ de saisie caché</source>
+      <source>Hidden input field</source>
       <translation>Champ de saisie caché</translation>
       
     </message>
     <message>
-      <source>Valeur</source>
+      <source>Value</source>
       <translation>Valeur</translation>
       
     </message>
     <message>
-      <source>Espaceur</source>
+      <source>Spacer</source>
       <translation>Espaceur</translation>
       
     </message>
     <message>
-      <source>Champ de saisie de texte</source>
+      <source>Text input field</source>
       <translation>Champ de saisie de texte</translation>
       
     </message>
     <message>
-      <source>Champ de saisie de chiffre</source>
+      <source>Number input field</source>
       <translation>Champ de saisie de chiffre</translation>
       
     </message>
     <message>
-      <source>Cochez cette case si l’entrée de ce champ de formulaire doit être validée.</source>
+      <source>Use this checkbox if the input of this form field should be validated.</source>
       <translation>Cochez cette case si l’entrée de ce champ de formulaire doit être validée.</translation>
       
     </message>
     <message>
-      <source>L’entrée nécessite une validation</source>
+      <source>Input requires validation</source>
       <translation>L’entrée nécessite une validation</translation>
       
     </message>
     <message>
-      <source>Champ de saisie case à cocher</source>
+      <source>Checkbox input field</source>
       <translation>Champ de saisie case à cocher</translation>
       
     </message>
     <message>
-      <source>Cochez cette case si la case doit être cochée par défaut.</source>
+      <source>Use this checkbox if the checkbox should be selected by default.</source>
       <translation>Cochez cette case si la case doit être cochée par défaut.</translation>
       
     </message>
     <message>
-      <source>Champ de saisie e-mail</source>
+      <source>Email input field</source>
       <translation>Champ de saisie e-mail</translation>
       
     </message>
     <message>
-      <source>Champ de saisie d’options</source>
+      <source>Options input field</source>
       <translation>Champ de saisie d’options</translation>
       
     </message>
     <message>
-      <source>Choisissez le type d’option</source>
+      <source>Choose option type</source>
       <translation>Choisissez le type d’option</translation>
       
     </message>
     <message>
-      <source>Choisissez comment afficher votre liste d’options dans le formulaire.</source>
+      <source>Choose how to display your option list in the form.</source>
       <translation>Choisissez comment afficher votre liste d’options dans le formulaire.</translation>
       
     </message>
     <message>
-      <source>Liste de boutons radio – 1 option peut être choisie</source>
+      <source>Radio button list - 1 option is choosable</source>
       <translation>Liste de boutons radio – 1 option peut être choisie</translation>
       
     </message>
     <message>
-      <source>Liste de cases à cocher – toutes les options peuvent être choisies</source>
+      <source>Checkbox list - all options are choosable</source>
       <translation>Liste de cases à cocher – toutes les options peuvent être choisies</translation>
       
     </message>
     <message>
-      <source>Menu déroulant – 1 option peut être choisie</source>
+      <source>Select box - 1 option is choosable</source>
       <translation>Menu déroulant – 1 option peut être choisie</translation>
       
     </message>
     <message>
-      <source>Menu déroulant – toutes les options peuvent être choisies</source>
+      <source>Select box - all options are choosable</source>
       <translation>Menu déroulant – toutes les options peuvent être choisies</translation>
       
     </message>
     <message>
-      <source>Ajouter option</source>
+      <source>Add option</source>
       <translation>Ajouter option</translation>
       
     </message>
     <message>
-      <source>Un clic sur ce bouton ajoute une option.</source>
+      <source>A click on this button adds one option.</source>
       <translation>Un clic sur ce bouton ajoute une option.</translation>
       
     </message>
     <message>
-      <source>Champ de saisie téléchargement</source>
+      <source>Upload input field</source>
       <translation>Champ de saisie téléchargement</translation>
       
     </message>
     <message>
-      <source>Options avec champ de saisie image</source>
+      <source>Options with image input field</source>
       <translation>Options avec champ de saisie image</translation>
       
     </message>
     <message>
-      <source>Ajouter un champ de formulaire</source>
+      <source>Add a form field</source>
       <translation>Ajouter un champ de formulaire</translation>
       
     </message>
     <message>
-      <source>chaîne</source>
+      <source>string</source>
       <translation>chaîne</translation>
       
     </message>
     <message>
-      <source>texte</source>
+      <source>text</source>
       <translation>texte</translation>
       
     </message>
     <message>
-      <source>chiffre</source>
+      <source>number</source>
       <translation>chiffre</translation>
       
     </message>
     <message>
-      <source>case à cocher</source>
+      <source>checkbox</source>
       <translation>case à cocher</translation>
       
     </message>
@@ -341,27 +338,27 @@
       
     </message>
     <message>
-      <source>options avec image</source>
+      <source>options with image</source>
       <translation>options avec image</translation>
       
     </message>
     <message>
-      <source>e-mail</source>
+      <source>email</source>
       <translation>e-mail</translation>
       
     </message>
     <message>
-      <source>télécharger</source>
+      <source>upload</source>
       <translation>télécharger</translation>
       
     </message>
     <message>
-      <source>caché</source>
+      <source>hidden</source>
       <translation>caché</translation>
       
     </message>
     <message>
-      <source>espaceur</source>
+      <source>spacer</source>
       <translation>espaceur</translation>
       
     </message>
@@ -371,43 +368,42 @@
       
     </message>
     <message>
-      <source>Ajouter élément</source>
+      <source>Add element</source>
       <translation>Ajouter élément</translation>
       
     </message>
     <message>
-      <source>Ajouter un nouvel élément de formulaire</source>
+      <source>Add a new form element</source>
       <translation>Ajouter un nouvel élément de formulaire</translation>
       
     </message>
     <message>
-      <source>Utilisation de captcha pour l’entrée.</source>
+      <source>Using captcha for input.</source>
       <translation>Utilisation de captcha pour l’entrée.</translation>
       
     </message>
     <message>
-      <source>Pas de captcha pour l’entrée.</source>
+      <source>No captcha for input.</source>
       <translation>Pas de captcha pour l’entrée.</translation>
       
     </message>
     <message>
-      <source>Veuillez remplir le formulaire. Les champs marqués d’un astérisque (*) sont obligatoires.</source>
+      <source>Please fill out the form. The input of fields which are marked with a asterisk (*) is required.</source>
       <translation>Veuillez remplir le formulaire. Les champs marqués d’un astérisque (*) sont obligatoires.</translation>
       
     </message>
   </context><context>
     <name>xrowformgenerator/kernel/classes/datatypes</name>
     <message>
-      <source>Entrée requise</source>
+      <source>Input required</source>
       <translation>Entrée requise</translation>
       
     </message>
   </context><context>
     <name>xrowformgenerator/mail</name>
     <message>
-      <location filename="xrowformgeneratortype.php" line="850"/>
-      <location line="0"/>
-      <source>N° :</source>
+      <location filename="xrowformgeneratortype.php" line="850"/> <location line="0"/>
+      <source>No.:</source>
       <translation>N° :</translation>
       
     </message>
@@ -418,36 +414,34 @@
       
     </message>
     <message>
-      <location filename="xrowformgeneratortype.php" line="910"/>
-      <location line="0"/>
-      <source>Oui</source>
+      <location filename="xrowformgeneratortype.php" line="910"/> <location line="0"/>
+      <source>Yes</source>
       <translation>Oui</translation>
       
     </message>
     <message>
-      <location filename="xrowformgeneratortype.php" line="914"/>
-      <location line="0"/>
-      <source>Non</source>
+      <location filename="xrowformgeneratortype.php" line="914"/> <location line="0"/>
+      <source>No</source>
       <translation>Non</translation>
       
     </message>
     <message>
-      <source>Formulaire envoyé avec succès</source>
+      <source>Form sent successfully</source>
       <translation>Formulaire envoyé avec succès</translation>
       
     </message>
     <message>
-      <source>Merci ! Vous avez soumis ce formulaire avec succès.</source>
+      <source>Thank You! You have successfully submitted the form.</source>
       <translation>Merci ! Vous avez soumis ce formulaire avec succès.</translation>
       
     </message>
     <message>
-      <source>Vos données</source>
+      <source>Your data</source>
       <translation>Vos données</translation>
       
     </message>
     <message>
-      <source>requis</source>
+      <source>required</source>
       <translation>requis</translation>
       
     </message>
