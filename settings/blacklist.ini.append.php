8496ccb1f064784ca1551374dae37e18fc23d92e
xinyuexrow <wang@xrow.de>
2014-11-05 16:21:16 +0100

    block Disposable Email Addresses

---
+++ settings/blacklist.ini.append.php
@@ -0,0 +1,18 @@
+<?php /* #?ini charset="iso-8859-1"?
+
+[SetBlackList]
+DomainsList[]
+DomainsList[]=mailinator.com
+DomainsList[]=pwrby.com
+DomainsList[]=guerrillamail.com
+DomainsList[]=fakeinbox.com
+DomainsList[]=bigprofessor.so
+DomainsList[]=yopmail.com
+DomainsList[]=dispostable.com
+DomainsList[]=trbvm.com
+DomainsList[]=tempmailer.com
+DomainsList[]=meltmail.com
+DomainsList[]=easytrashmail.com
+DomainsList[]=bounceio.com
+
+*/ ?>
